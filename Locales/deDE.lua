8d27724190c41fbff2b76f11ca1b7fdf10220b2e
Leo Melnyk <leonkmelnyk@gmail.com>
2021-05-24 12:41:40 -0600

    Update DE and RU translations
    Importing localization updates for RU and DE from the community.

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -1,90 +1,47 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("BGHistorian", "deDE", true)
 
---[[Translation missing --]]
 L["%F %T"] = "%F %T"
---[[Translation missing --]]
 L["%ih %im"] = "%ih %im"
---[[Translation missing --]]
 L["%im %is"] = "%im %is"
---[[Translation missing --]]
 L["%is"] = "%is"
 L["Alterac Valley"] = "Alteractal"
 L["Arathi Basin"] = "Arathibecken"
---[[Translation missing --]]
-L["Eye of the Storm"] = "Eye of the Storm"
---[[Translation missing --]]
-L["Avg"] = "Avg"
---[[Translation missing --]]
-L["Battleground"] = "Battleground"
---[[Translation missing --]]
-L["Battlegrounds records can impact memory usage (0 means unlimited)"] = "Battlegrounds records can impact memory usage (0 means unlimited)"
---[[Translation missing --]]
-L["Cleanup and optimize collected data"] = "Cleanup and optimize collected data"
---[[Translation missing --]]
-L["Database reset"] = "Database reset"
---[[Translation missing --]]
-L["Database Settings"] = "Database Settings"
---[[Translation missing --]]
-L["Date"] = "Date"
---[[Translation missing --]]
-L["Deaths"] = "Deaths"
---[[Translation missing --]]
-L["Delete all collected data"] = "Delete all collected data"
---[[Translation missing --]]
-L["Drag"] = "Drag"
---[[Translation missing --]]
-L["Duration"] = "Duration"
---[[Translation missing --]]
-L["HK"] = "HK"
---[[Translation missing --]]
-L["Honor"] = "Honor"
---[[Translation missing --]]
-L["Honorable Kills"] = "Honorable Kills"
---[[Translation missing --]]
-L["KB"] = "KB"
---[[Translation missing --]]
-L["Killing Blows"] = "Killing Blows"
---[[Translation missing --]]
-L["Left Click"] = "Left Click"
---[[Translation missing --]]
-L["Maximum history records"] = "Maximum history records"
---[[Translation missing --]]
-L["Minimap Button Settings"] = "Minimap Button Settings"
---[[Translation missing --]]
-L["Optimize database"] = "Optimize database"
---[[Translation missing --]]
-L["Options"] = "Options"
---[[Translation missing --]]
-L["Purge database"] = "Purge database"
---[[Translation missing --]]
-L["Recorded %i battlegrounds"] = "Recorded %i battlegrounds"
---[[Translation missing --]]
-L["Right Click"] = "Right Click"
---[[Translation missing --]]
-L["Show minimap button"] = "Show minimap button"
---[[Translation missing --]]
-L["Sum"] = "Sum"
---[[Translation missing --]]
-L["to move this button"] = "to move this button"
---[[Translation missing --]]
-L["to open options"] = "to open options"
---[[Translation missing --]]
-L["to open the main window"] = "to open the main window"
-L["Warsong Gulch"] = "Warsongschlucht"
---[[Translation missing --]]
-L["Winner"] = "Winner"
---[[Translation missing --]]
-L["Winrate"] = "Winrate"
---[[Translation missing --]]
-L["Damage Done"] = "Damage Done"
---[[Translation missing --]]
-L["Damage"] = "Damage"
---[[Translation missing --]]
-L["Healing Done"] = "Healing Done"
---[[Translation missing --]]
-L["Healing"] = "Healing"
---[[Translation missing --]]
+L["Avg"] = "Durchschn."
+L["Battleground"] = "Schlachtfeld"
+L["Battlegrounds records can impact memory usage (0 means unlimited)"] = "Schlachtfeldaufzeichnungen können sich auf die Arbeitsspeichernutzung auswirken (0 bedeutet unbegrenzt)"
+L["Cleanup and optimize collected data"] = "Bereinige und optimiere die gesammelten Daten "
+L["Damage"] = "Schaden"
+L["Damage Done"] = "Verursachter Schaden"
+L["Database reset"] = "Datenbank zurücksetzen"
+L["Database Settings"] = "Datenbank Einstellungen"
+L["Date"] = "Datum"
+L["Deaths"] = "Tode"
+L["Delete all collected data"] = "Lösche alle gesammelten Daten"
+L["Drag"] = "Ziehen "
+L["Duration"] = "Dauer"
+L["Eye of the Storm"] = "Auge des Sturms"
+L["Healing"] = "Heilung"
+L["Healing Done"] = "Gewirkte Heilung"
+L["HK"] = "ES"
+L["Honor"] = "Ehre"
+L["Honorable Kills"] = "Ehrenhafte Siege"
+L["KB"] = "TS"
+L["Killing Blows"] = "Todesstöße"
+L["Left Click"] = "Linksklick"
+L["Maximum history records"] = "Maximale Schlachtfeldaufzeichnungen "
+L["Minimap Button Settings"] = "Minimap Button Einstellung "
+L["Opens or closes the main window"] = "Öffnet oder schließt das Hauptfenster"
+L["Optimize database"] = "Datenbank optimieren "
+L["Options"] = "Optionen"
+L["Purge database"] = "Datenbank löschen"
+L["Recorded %i battlegrounds"] = "%i Schlachtfelder aufgezeichnet"
+L["Right Click"] = "Rechtsklick"
+L["Show minimap button"] = "Minimap Button anzeigen"
+L["Sum"] = "Summe"
+L["to move this button"] = "um diesen Button zu verschieben "
+L["to open options"] = "um Optionen zu öffnen"
+L["to open the main window"] = "um das Hauptfenster zu öffnen "
 L["Toggle"] = "Toggle"
---[[Translation missing --]]
-L["Opens or closes the main window"] = "Opens or closes the main window"
-
+L["Warsong Gulch"] = "Kriegshymnenschlucht"
+L["Winner"] = "Gewinner"
+L["Winrate"] = "Gewinnrate"
